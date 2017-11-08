c46c83d534cb4077480bc911bd7d003eb31511d7
UrbanoFonseca <francisco.urbfonseca@gmail.com>
2017-11-08 13:12:38 +0000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='confusion_matrix_cv',
+      version='0.1.1',
+      description='Confusion Matrix Cross Validation',
+      url='https://github.com/UrbanoFonseca/confusion_matrix_cv',
+      author='UrbanoFonseca',
+      license='GPLv3',
+      packages=['confusion_matrix_cv'],
+      zip_safe=False)
